5d483aab7680703e5e0aefac38b2a8cae90ae0bb
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-10-26 11:30:14 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import mermaid from "astro-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://riccardodivirgilio.github.io",
+  base: "/learning-zio",
   integrations: [
     starlight({
       title: "Lezioni di Programmazione",
